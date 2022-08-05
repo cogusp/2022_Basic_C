0c5156a914dde4590a7b5a118b1c605007ec9613
cogusp <cogusp1@gmail.com>
2022-08-05 23:03:59 +0900

    10-2; 배열을 처리하는 함수

---
+++ 10-2/10-challenge/10-challenge.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void input_nums(int* lotto_nums);		// 배열에 로또 번호를 입력하는 함수
+void print_nums(int* lotto_nums);		// 배열에 저장된 값을 출력하는 함수
+
+int main(void)
+{
+	int lotto_nums[6];					// 로또 번호를 저장할 배열
+
+	input_nums(lotto_nums);				// 입력 함수 호출
+	print_nums(lotto_nums);				// 출력 함수 호출
+
+	return 0;
+}
+
+void input_nums(int* lotto_nums)
+{
+	
+}
+
+void print_nums(int* lotto_nums)
+{
+
+}
